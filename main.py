8952f43b9a3ffa0bc477f426de5928ce80f32356
toxot <a.a.sarkisov@gmail.com>
2020-06-04 03:50:06 +0300

    add v2

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ import urllib.request
 token=os.getenv('TG_TOKEN') or ''
 port=os.getenv('TGAPI_PORT') or '8001'
 auth_key=os.environ.get('API_AUTHKEY') or 'p@ssw0rd'
-limit = 1000
+limit = 3000
 
 def text_splitter(text, limit):
     return [text[i: i + limit] for i in range(0, len(text), limit)]
 
+
 def sendText(chat,text):
     url = "https://api.telegram.org/bot"+token+"/sendMessage";
     data = {'chat_id' : chat, 'text': text}
-    r= requests.post(url, data=data, timeout=10)
-    print(r.status_code, r.reason, r.content)
-    data= " status: "+str(r.status_code)+" reason:"+ str(r.reason) +" content:"+ str(r.content)
-    return data
+    try:
+        r= requests.post(url, data=data, timeout=10)
+        print(r.status_code, r.reason, r.content)
+        data= " status: "+str(r.status_code)+" reason:"+ str(r.reason) +" content:"+ str(r.content)
+        return data
+    except Exception as e:
+        raise e
 
 @post('/message') # or @route('/message', method='POST')
 def login_submit():
@@ -36,5 +40,22 @@ def login_submit():
     else:
     	abort(403, "NOAUTH")
     #return template('{{data}}', data=text )
+
+@post('/messagev2') # or @route('/message', method='POST')
+def login_submit():
+    dict = request.forms
+    text=''
+    for key in dict:
+        if key != 'key' and key != 'recepient':
+            text=text+"\n"+key+": "+dict[key]
+    akey = request.forms.get('key')
+    recepient = request.forms.get('recepient')
+    if akey == auth_key:
+        for txt in text_splitter(text,limit):
+           sendText(recepient,txt)
+    else:
+        abort(403, "NOAUTH")
+    #return template('{{data}}', data=text )
+
 
 run(host='', port=port, server='gevent')
